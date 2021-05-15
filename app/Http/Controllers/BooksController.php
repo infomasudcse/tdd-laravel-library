c69d0bf969afe5b18086b3641ad96a035253a6c7
Md Masud <infomasud@gmail.com>
2021-05-16 00:10:00 +0600

    Book info created

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function store(){
+       
+        Book::create($this->validateRequest());
+    }
+
+    public function update(Book $book){
+       
+        $book->update($this->validateRequest());    
+
+    }
+
+    public function validateRequest(){
+        $data = request()->validate([
+            'title'=>'required',
+            'author'=>'required'
+        ]);
+        return $data;
+    }
+
+
+}
